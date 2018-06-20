eed8a609b87ae9333ff0452d271400e497656c20
Karol <karol.sawka@topmaker.pl>
2018-06-20 22:53:45 +0200

    admin modal window

--- includes/Invoice_Box.php
+++ includes/Invoice_Box.php
@@ -52,7 +52,7 @@ if ( !class_exists( 'FI_Invoice_Box') ) {
      
      
         /**
-         * Rendere the invoice meta box
+         * Render the invoice meta box
          *
          * @since 0.0.1
          */
@@ -62,18 +62,43 @@ if ( !class_exists( 'FI_Invoice_Box') ) {
             $order = new WC_Order($post->ID);
             ?>
                 <div id="fi-invoice-form">
-                    <?php wp_nonce_field( 'custom_nonce_action', 'infakt_invoice_nonce' ); ?>
-                    <input type="hidden" name="first_name" value="<?php echo $order->data['billing']['first_name']; ?>">
-                    <input type="hidden" name="last_name" value="<?php echo $order->data['billing']['last_name']; ?>">
-                    <input type="hidden" name="company" value="<?php echo $order->data['billing']['company']; ?>">
-                    <input type="hidden" name="address_1" value="<?php echo $order->data['billing']['address_1']; ?>">
-                    <input type="hidden" name="address_2" value="<?php echo $order->data['billing']['address_2']; ?>">
-                    <input type="hidden" name="postcode" value="<?php echo $order->data['billing']['postcode']; ?>">
-                    <input type="hidden" name="city" value="<?php echo $order->data['billing']['city']; ?>">
-                    <input type="hidden" name="state" value="<?php echo $order->data['billing']['state']; ?>">
-                    <input type="hidden" name="country" value="<?php echo $order->data['billing']['country']; ?>">
-                    <button name="fi-new-invoice" id="fi-new-invoice" class="button"><?php _e( 'Wystaw fakturę', 'fi' ); ?></button>
+                    <a href="#TB_inline?width=600&height=800&inlineId=fi-modal" class="button thickbox"><?php _e( 'Wystaw fakturę', 'fi' ); ?></a>
+                    <div id="fi-modal" style="display:none;">
+                        <h2><?php _e( 'Wystaw fakturę w Infakt.pl', 'fi' ); ?></h2>
+                        <?php wp_nonce_field( 'custom_nonce_action', 'infakt_invoice_nonce' ); ?>
+                        <p class="form-field">
+                            <label for="first_name"><?php _e( 'Imię', 'fi' ); ?></label> <input type="text" name="first_name" value="<?php echo $order->data['billing']['first_name']; ?>">
+                        </p>    
+                        <p class="form-field">
+                            <label for="last_name"><?php _e( 'Nazwisko', 'fi' ); ?></label> <input type="text" name="last_name" value="<?php echo $order->data['billing']['last_name']; ?>">
+                        </p>    
+                        <p class="form-field">
+                            <label for="company"><?php _e( 'Firma', 'fi' ); ?></label> <input type="text" name="company" value="<?php echo $order->data['billing']['company']; ?>">
+                        </p>    
+                        <p class="form-field">
+                            <label for="address_1"><?php _e( 'Adres 1', 'fi' ); ?></label> <input type="text" name="address_1" value="<?php echo $order->data['billing']['address_1']; ?>">
+                        </p>    
+                        <p class="form-field">
+                            <label for="address_2"><?php _e( 'Adres 2', 'fi' ); ?></label> <input type="text" name="address_2" value="<?php echo $order->data['billing']['address_2']; ?>">
+                        </p>    
+                        <p class="form-field">
+                            <label for="postcode"><?php _e( 'Kod pocztowy', 'fi' ); ?></label> <input type="text" name="postcode" value="<?php echo $order->data['billing']['postcode']; ?>">
+                        </p>    
+                        <p class="form-field">
+                            <label for="city"><?php _e( 'Miejscowość', 'fi' ); ?></label> <input type="text" name="city" value="<?php echo $order->data['billing']['city']; ?>">
+                        </p>    
+                        <p class="form-field">
+                            <label for="state"><?php _e( 'Województwo', 'fi' ); ?></label> <input type="text" name="state" value="<?php echo $order->data['billing']['state']; ?>">
+                        </p>    
+                        <p class="form-field">
+                            <label for="country"><?php _e( 'Kraj', 'fi' ); ?></label> <input type="text" name="country" value="<?php echo $order->data['billing']['country']; ?>">
+                        </p>
+                        <p class="form-field">
+                            <button name="fi-new-invoice" id="fi-new-invoice" class="button"><?php _e( 'Wystaw fakturę', 'fi' ); ?></button>
+                        </p>
+                    </div>
                 </div>
+                
             <?php
             
         }
